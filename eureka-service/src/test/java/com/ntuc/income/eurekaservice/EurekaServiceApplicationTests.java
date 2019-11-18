6ccfd5760f942deba09ff6062e859aab6efc472c
Mukesh Kumar <mukeshkumar@Mukesh-Kumars-MacBook-Pro.local>
2019-11-18 15:56:19 +0800

    Adding the eureka service.

---
+++ eureka-service/src/test/java/com/ntuc/income/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntuc.income.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
